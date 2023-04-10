0cc13e5bebca383c20179be6e8159333905e665f
{Karasun} <{diego.torregano@gmail.com}>
2023-04-10 15:34:27 +0200

    modif css presentation produit

--- script/script.js
+++ script/script.js
@@ -2,11 +2,11 @@ const root = document.querySelector(":root");
 const toggle = document.getElementById("toggle-theme");
 
 toggle.addEventListener("click", () => {
-  if (root.style.getPropertyValue("--background-color") === "#1e1e1e") {
-    root.style.setProperty("--background-color", "#fff");
+  if (root.style.getPropertyValue("--background-color") === "#C5C4C4") {
+    root.style.setProperty("--background-color", "#1e1e1e");
     root.style.setProperty("--text-color", "#000");
   } else {
-    root.style.setProperty("--background-color", "#1e1e1e");
+    root.style.setProperty("--background-color", "#C5C4C4");
     root.style.setProperty("--text-color", "#fff");
   }
 });
